922c69bc024d612f2efd2ace1c9f405b4de271ca
Harish Ramachandran <harishsr@icloud.com>
2017-07-16 11:53:16 -0400

    As per latest Vue requirements, give v-for a :key

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
+    <h1>Harish's Todo List</h1>
     <p>Completed Tasks: {{ todos.filter(todo => {return todo.done === true }).length}}</p>
     <p>Pending Tasks: {{ todos.filter(todo => {return todo.done === false }).length}}</p>
 
-    <todo v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:index="index"></todo>
+    <todo v-for="todo in todos" v-bind:todo="todo" :key="todo.id"></todo>
   </div>
 </template>
 
